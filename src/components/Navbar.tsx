c070474f278583bd9bbd4a2b46ad09f1292c7e8e
Quique1912 <quique190120@gmail.com>
2025-03-10 08:26:46 +0100

    version casi casi final sin css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,39 +5,14 @@ function Navbar() {
   const {user, isAdmin, isAuthenticated, logout} = useAuth()
 
   return (
-<<<<<<< HEAD
-    <nav className="bg-red-800 border-gray-200 shadow-md w-full">
-      <div className="w-full flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="../src/assets/logo.png" className="h-8" alt="Logo" />
-          <span className="self-center text-2xl font-semibold text-white">Quique19</span>
-        </a>
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button type="button" className="text-white bg-red-600 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2">Get started</button>
-        </div>
-        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-red-700 rounded-lg bg-red-900 md:flex-row md:mt-0 md:border-0 md:bg-red-800">
-            <li><Link to="/" className="block py-2 px-3 text-white hover:bg-red-700 rounded-md">Home</Link></li>
-            <li><Link to="/login" className="block py-2 px-3 text-white hover:bg-red-700 rounded-md">Login</Link></li>
-            <li><Link to="/register" className="block py-2 px-3 text-white hover:bg-red-700 rounded-md">Registro</Link></li>
-            <li><Link to="/profile" className="block py-2 px-3 text-white hover:bg-red-700 rounded-md">Profile</Link></li>
-            <li><Link to="/userList" className="block py-2 px-3 text-white hover:bg-red-700 rounded-md">Usuarios</Link></li>
-            <li><Link to="/offers" className="block py-2 px-3 text-white hover:bg-red-700 rounded-md">Ofertas</Link></li>
-            <li><Link to="/categories" className="block py-2 px-3 text-white hover:bg-red-700 rounded-md">Categorias</Link></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
-=======
     
 
 <nav className="bg-white border-gray-200 dark:bg-gray-900">
 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
   <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"/>
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+      <img src="../src/assets/logo.png" className="h-8" alt="Flowbite Logo"/>
+      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">QUIQUE19</span>
   </a>
   <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
       <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
@@ -89,7 +64,6 @@ function Navbar() {
 </nav>
 
   )
->>>>>>> 46d0174 (version casi casi final sin css)
 }
 
 export default Navbar
