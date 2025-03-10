6dd27bcaf1c18af18616a20b47a2be4ac0772d2b
Quique1912 <quique190120@gmail.com>
2025-03-10 09:55:53 +0100

    casi conseguido

--- src/utils/FetchAPI.ts
+++ src/utils/FetchAPI.ts
@@ -1,35 +1,28 @@
 export const fetchAPI = async (endPoint: string, options = {}) => {
-    try {
+  try {
       const response = await fetch(endPoint, options);
-  
-      if (response.status == 401) {
-        window.location.href = "/login";
-        throw new Error("Sesión expirada. Inicia sesión nuevamente");
+
+      // Si la respuesta es 401 (sesión expirada), redirigir a la página de login
+      if (response.status === 401) {
+          // Puedes eliminar la sesión si es necesario aquí también
+          window.location.href = "/login"; // Redirigir a login
+          throw new Error("Sesión expirada. Inicia sesión nuevamente");
       }
-  
-      /*  if (!response.ok) {
-              const errorData = await response.json().catch(() => null)
-              throw new Error(errorData?.message || 'Error desconocido')
-          } */
-  
+
       const jsonData = await response.json();
       if (!response.ok) {
-        if (jsonData.error) {
-          throw jsonData.error;
-        } else if (jsonData.message) {
-          throw jsonData.message;
-        } else {
-          throw { error: jsonData };
-        }
+          if (jsonData.error) {
+              throw jsonData.error;
+          } else if (jsonData.message) {
+              throw jsonData.message;
+          } else {
+              throw { error: jsonData };
+          }
       }
-  
+
       return jsonData;
-    } catch (error) {
-      /* catch (error) {
-          const msg = error instanceof Error ? error.message : 'Error desconocido'
-          throw new Error(msg)
-      } */
-      console.error("error:", error);
+  } catch (error) {
+      console.error("Error:", error);
       throw error;
-    }
-  };
+  }
+};
